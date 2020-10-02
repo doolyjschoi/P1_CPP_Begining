ba6dba33de737ecd9aa4f78f94f44fbdf690c65a
wDooJSC <doolyjschoi@hanmail.net>
2020-10-02 13:46:51 +0900

    modify main

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
+// first main function
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
 
-int main() {
-    printf("hello, world!");
+struct Vector2D{
+	int x;
+	int y;
 
-    return 0;
+	void print() {
+		printf("%d %d\n", x, y);
+	}
+};
+
+std::ostream &operator << (std::ostream &stream, Vector2D obj)
+{
+	stream << obj.x << " " << obj.y << std::endl;
+
+	return stream;
+}
+
+void main() {
+
+	Vector2D my_vector;
+	my_vector.x = 1;
+	my_vector.y = 2;
+
+	//FILE *of = fopen("cstyle.txt", "w");
+	//fprintf(of, "%d %d", my_vector.x, my_vector.y);
+	//fclose(of);
+
+	//my_vector.print();
+	
+	//printf("This is c style!\n");
+	/*using namespace std;*/
+	//std::cout << "This is c++ style!" << std::endl;
+	//std::cout << my_vector.x << " " << my_vector.y << std::endl;
+
+	std::cout << my_vector << std::endl;
+
+	std::ofstream ofile("cppstyle.txt");
+	ofile << my_vector << std::endl;
+	
 }
