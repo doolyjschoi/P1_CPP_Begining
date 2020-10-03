6a08a7cdce66f866342a132929e37a554337b734
doolyHome <doolyjschoi@naver.com>
2020-10-04 08:43:26 +0900

    Vector calculation text

--- Student_EXE/Student_EXE/Student1.cpp
+++ Student_EXE/Student_EXE/Student1.cpp
@@ -24,21 +24,37 @@ public:
 		y = 0.0f;
 		z = 0.0f;
 	}
-	friend std::ostream& operator<< (std::ostream& out, const Vector3D& vector3D);
+	Vector3D& operator=(const Vector3D& rhs) // compound assignment (does not need to be a member,
+	{
+		this->x = rhs.x;
+		this->y = rhs.y;
+		this->z = rhs.z;
+		return *this; // return the result by reference
+	}
+	// passing lhs by value helps optimize chained a+b+c otherwise, both parameters may be const references
+	friend Vector3D operator+(Vector3D lhs,	const Vector3D& rhs) 
+	{
+		lhs.x += rhs.x; // reuse compound assignment
+		lhs.y += rhs.y;
+		lhs.z += rhs.z;
+		return lhs; // return the result by value (uses move constructor)
+	}
+
+	friend std::ostream& operator<< (std::ostream& os, const Vector3D& vc);
 };
 
-std::ostream& operator<< (std::ostream& out, const Vector3D& vector3D)
+std::ostream& operator<< (std::ostream& os, const Vector3D& vc)
 {
-	out << "Vector3D(" << vector3D.x << ", " << vector3D.y << ", " << vector3D.z << ")";
+	os << "Vector3D(" << vc.x << ", " << vc.y << ", " << vc.z << ")";
 
-	return out;
+	return os;
 }
 
 void main()
 {
 	Vector3D a(1, 2, 3), b, c;
 	b = Vector3D(4, 5, 6);
-	//c = a + b;
+	c = a + b;
 
-	std::cout << c << std::endl;
+	std::cout << a << " + " << b << " = "  << c << std::endl;
 }
